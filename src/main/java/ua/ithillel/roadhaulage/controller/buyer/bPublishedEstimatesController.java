7f95429008c373203a4e1ad4c11d26701308bb3d
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2024-12-25 17:40:28 +0200

    Removing repetitive html code

--- src/main/java/ua/ithillel/roadhaulage/controller/buyer/bPublishedEstimatesController.java
+++ src/main/java/ua/ithillel/roadhaulage/controller/buyer/bPublishedEstimatesController.java
@@ -1,17 +1,28 @@
 package ua.ithillel.roadhaulage.controller.buyer;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.ithillel.roadhaulage.entity.User;
+import ua.ithillel.roadhaulage.service.interfaces.EstimateService;
 
 @Controller
 @RequestMapping("/buyer/account/estimates/published")
 @AllArgsConstructor
 public class bPublishedEstimatesController {
+    private EstimateService estimateService;
 
     @GetMapping
-    public String publishedEstimatesPage() {
+    public String publishedEstimatesPage(@AuthenticationPrincipal User user,
+                                         Model model) {
+
         return "buyer/estimates/published";
     }
+
 }
