0f44cffd31329ca40914d9f503356f8e063e3d4c
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2025-03-01 15:38:38 +0200

    feat: implementet JWT-based authentication

---
+++ src/main/java/ua/ithillel/roadhaulage/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package ua.ithillel.roadhaulage.filter;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ua.ithillel.roadhaulage.dto.AuthUserDto;
+import ua.ithillel.roadhaulage.service.interfaces.UserService;
+import ua.ithillel.roadhaulage.util.JwtUtil;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            try {
+                Claims claims = jwtUtil.parseToken(token);
+                username = claims.getSubject();
+            } catch (Exception e) {
+                logger.error("Invalid JWT token", e);
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            AuthUserDto user = (AuthUserDto) userService.loadUserByUsername(username);
+            if (user.isEnabled()) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
